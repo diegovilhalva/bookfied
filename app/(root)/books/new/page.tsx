e73d63779717508ee0603d0f48a037c78b4c3829
Diego Vilhalva <diegorocha450@yahoo.com>
2026-03-09 08:56:19 -0300

    update pages

--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -1,16 +1,26 @@
 import UploadForm from "@/components/UploadForm"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
 
+const Page = async () => {
+  const { userId } = await auth()
 
-const Page = () => {
-    return (
-        <main className="new-book">
-            <section className="flex flex-col gap-5 text-center">
-                <h1 className="page-title-xl">Add a New Book</h1>
-                <p className="subtitle">Upload a PDF to generate your  interactive reading experience</p>
-            </section>
-            <UploadForm />
-        </main>
-    )
+  if (!userId) {
+    redirect("/")
+  }
+
+  return (
+    <main className="new-book">
+      <section className="flex flex-col gap-5 text-center">
+        <h1 className="page-title-xl">Add a New Book</h1>
+        <p className="subtitle">
+          Upload a PDF to generate your interactive reading experience
+        </p>
+      </section>
+
+      <UploadForm />
+    </main>
+  )
 }
 
 export default Page
